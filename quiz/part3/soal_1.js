71724b251239c25f7ac2b04734ea6cd4ad03f4fc
qaizzzthh <202410715117@mhs.ubharajaya.ac.id>
2025-02-22 19:36:27 +0700

    part3

---
+++ quiz/part3/soal_1.js
@@ -0,0 +1,49 @@
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax while. 
+// Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
+// menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+//tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+//Code
+console.log("\nLOOPING PERTAMA (menghitung maju)\n")
+let i=1;
+while (i<=10) {
+    console.log(i)
+    i++
+}
+console.log("\nLOOPING KEDUA (menghitung mundur)\n")
+let j=10;
+while (j>=1) {
+    console.log(j)
+    j--
+}
+
+//Output
+/* 
+LOOPING PERTAMA (menghitung maju)
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+LOOPING KEDUA (menghitung mundur)
+
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+
+*/
---
+++ quiz/part3/soal_2.js
@@ -0,0 +1,54 @@
+// Problem
+// Pada tugas ini kamu diminta untuk melakukan looping dalam JavaScript dengan menggunakan syntax for. 
+// Untuk membuat tantangan ini lebih menarik, kamu juga diminta untuk membuat suatu looping yang menghitung maju dan 
+// menghitung mundur. Jangan lupa tampilkan di console juga judul 'LOOPING PERTAMA' dan 'LOOPING KEDUA'.
+//tampilkan output yang menunjukan kalau looping itu maju pada looping pertama dan mundur pada looping kedua
+
+//Code
+console.log("\nLOOPING PERTAMA (menghitung maju)\n")
+for (let i = 1; i <= 10; i++) {
+    console.log(i)
+}
+console.log("\nLOOPING KEDUA (menghitung mundur)\n")
+for(let j=20; j>=1;j--){
+    console.log(j)
+}
+
+//Output
+/* 
+LOOPING PERTAMA (menghitung maju)
+
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+LOOPING KEDUA (menghitung mundur)
+
+20
+19
+18
+17
+16
+15
+14
+13
+12
+11
+10
+9
+8
+7
+6
+5
+4
+3
+2
+1
+*/
